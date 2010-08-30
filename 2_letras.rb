7b24f0b68e2a267397b97b4054727c40c0a983a4
Anderson Dias <andersondaraujo@gmail.com>
2010-08-30 07:51:12 -0700

    Exemplos de strings em múltiplas linhas e de utilização de alguns métodos

--- 2_letras.rb
+++ 2_letras.rb
@@ -38,3 +38,55 @@ puts 'up\down'
 
 puts "1 + 2 = #{1 + 2}"
 puts '1 + 2 = #{1 + 2}'
+
+#6 -> Strings em múltiplas linhas
+
+print <<HERE
+Double quoted \
+here document.
+It is #{Time.now}
+HERE
+
+print <<-’THERE’
+  This is a single quoted.
+  The above used #{Time.now}
+  THERE
+
+#7 -> Métodos úteis
+
+frase = "O rato roeu a roupa do rei de roma"
+puts frase.gsub("r", "p")
+puts frase
+frase.gsub!("r", "p")
+puts frase
+
+nome = "Anderson Dias de Araujo Ferreira"
+puts nome.upcase
+puts nome
+nome.upcase!
+puts nome
+
+nome.downcase!
+puts nome
+nome.capitalize!
+puts nome
+nome.swapcase!
+puts nome
+
+puts "string com 20 letras".size
+puts "string com 20 letras".length
+
+puts "oooooooooooooowww man!".squeeze
+
+titulo = "Titulo".center(50)
+puts titulo.inspect
+puts titulo.lstrip.inspect
+puts titulo.rstrip.inspect
+puts titulo.strip.inspect
+
+titulo.strip!
+puts titulo.reverse
+
+"Não Vazia".empty?
+" ".empty?
+"".empty?
